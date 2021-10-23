898f5f0ee1f445d70ce80211a6092a128a22696c
priyanshusingh305 <singhpriyanshu305@gmail.com>
2021-10-23 20:36:48 +0530

    cool

---
+++ linkedin bot.py
@@ -0,0 +1,48 @@
+import os
+from time import sleep
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+ 
+# linkedin post liker
+#
+# Add your post url (if any) to your posts here for others to like them : https://docs.google.com/document/d/1YiPKlYkzGMO08HfpZuOZ-S4B-t6KrA7-Xe1x1ElFmnk/edit?usp=sharing
+#
+#
+#
+# To like other people's post:
+# step 0 : you need chrome for this. Also install modules :
+# pip install selenium && pip install webdriver_manager && pip install tinydb
+ 
+# step 1 change this from : https://docs.google.com/document/d/1YiPKlYkzGMO08HfpZuOZ-S4B-t6KrA7-Xe1x1ElFmnk/edit?usp=sharing
+links = [
+   "https://www.linkedin.com/mynetwork/invitation-manager/"
+   ]
+ 
+
+ 
+options = webdriver.ChromeOptions()
+options.add_argument("user-data-dir={}\driver_data".format(os.getcwd()))
+ 
+driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)
+ 
+driver.get("https://linkedin.com")
+# delete this after every month
+ 
+while "1" != input("press 1 when signed in: "):
+    pass
+for link in links:
+    try:
+        print("accessing link ", link)
+        driver.get(link)
+        sleep(2)
+        Accept_button = driver.find_elements_by_xpath("//button[contains(@class,'artdeco-button artdeco-button--2 artdeco-button--secondary ember-view invitation-card__action-btn')]")
+        if (Accept_button==0):
+            print("Already Done!")
+        else:    
+            for accept in Accept_button:
+                accept.click()
+            print("Accepted")
+    except Exception as e:
+        print("error processing link\nlink: ", link, "\nerror",  e)
+ 
+driver.close()
